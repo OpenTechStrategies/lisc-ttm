f4b78ab43dbd7505342a64dd2e8df017cdfbf22b
cecilia-donnelly <donnellycecilia1@gmail.com>
2014-06-06 14:33:39 -0400

    Add new footer.
    Added a Contact Us page in the global include folder.
    Added link to that page in the footer and changed stylesheet
    to display it correctly in the footer.  The contact page
    will send mail through a form to our help address at OTS.

--- footer.php
+++ footer.php
@@ -9,6 +9,8 @@ Footer, including a link to the helpdesk email.
 <div id="footer">
 <span><a href="http://opentechstrategies.com"
       ><img src="/images/ots-logo-name.png" id="ots_logo" /></a></span>
+       <span id="helplink" class="helptext" ><a href="<?php echo '/include/contactus.php';?>">Questions? Comments? Click here.</a></span>
+      
 <span><a href="http://chapinhall.org/"
       ><img src="/images/chapin-hall-logo.png" id="ch_logo" /></a></span>
 </div>
