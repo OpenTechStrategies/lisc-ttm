be877d349a46aea1914ef2c22d1c92c5050619ea
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2014-06-11 12:37:55 -0500

    Simplify password_hash for testing.

--- ajax/hash_password.php
+++ ajax/hash_password.php
@@ -2,7 +2,8 @@
 require("../include/phpass-0.3/PasswordHash.php");
 $hasher=new PasswordHash(8, false);
 
-$get_plaintext_passes="SELECT User_ID, User_Password FROM Users";
+
+/*$get_plaintext_passes="SELECT User_ID, User_Password FROM Users";
 include "../include/dbconnopen.php";
 $plaintext_passes=mysqli_query($cnnLISC, $get_plaintext_passes);
 while ($plainpass=mysqli_fetch_row($plaintext_passes)){
@@ -10,7 +11,11 @@ while ($plainpass=mysqli_fetch_row($plaintext_passes)){
     $save_hashed="UPDATE Users SET User_Password='$hashpass' WHERE User_ID='$plainpass[0]'";
     mysqli_query($cnnLISC, $save_hashed);
 }
+*/
 
-include "../include/dbconnclose.php";
+/*
+include "../include/dbconnclose.php";*/
+$hashpass=$hasher->HashPassword('password');
+echo $hashpass . "\n";
 
 ?>
