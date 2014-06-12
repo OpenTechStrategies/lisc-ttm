67de7c09f7f79184bca58bbf32f3f9f9d805e8d3
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2014-06-12 10:30:09 -0500

    Test password hashing and checking. \n \n Hash an array of passwords and check the hash against the entered plaintext passwords.  Both hashing and checking work properly.

--- ajax/hash_password.php
+++ ajax/hash_password.php
@@ -15,7 +15,30 @@ while ($plainpass=mysqli_fetch_row($plaintext_passes)){
 
 /*
 include "../include/dbconnclose.php";*/
+
+/*hash a sample password*/
 $hashpass=$hasher->HashPassword('password');
 echo $hashpass . "\n";
+
+/*check to see if hash matches the plaintext*/
+$check=$hasher->CheckPassword('password', $hashpass);
+echo $check . "\n";
+
+
+/*now that we've established that works, do this for an array of passwords*/
+
+$password_array=array('password', 'test', '123456', 'c0mb0numl3t');
+foreach ($password_array as $pass){
+    $hashpass_array[]=$hasher->HashPassword($pass);
+}
+print_r($hashpass_array);
+
+$n=0;
+
+while($n<count($password_array)){
+    $check_array[]=$hasher->CheckPassword($password_array[$n], $hashpass_array[$n]);
+    $n++;
+}
+print_r($check_array);
 
 ?>
