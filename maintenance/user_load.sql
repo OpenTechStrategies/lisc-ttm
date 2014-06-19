2da9a083f938aaa636301addc0aa2ae8d21de2e0
Karl Fogel <kfogel@red-bean.com>
2014-06-19 17:39:35 -0500

    Improve password hashing-and-updating script.
    * maintenance/hash_password.php: Rename to...
    
    * maintenance/passwords/update_passwords.php: ... here.  Redocument.
      Use stdin and stdout instead of named files.  Make all errors fatal.
      Check for correct number of arguments.  Use "|" instead of comma as
      the separator between username and password.  Handle blank lines and
      trailing newline in input.  Emit output during loop instead of
      saving and emitting at end.  Remove needless counter variable.
      Reformat to abide by the 80-column line convention.
    
    * maintenance/test.txt: Rename to...
    
    * maintenance/passwords/usermap_file_example.txt: ... here.
    
    * maintenance/user_load.sql: Removed.

--- maintenance/user_load.sql
+++
@@ -1,8 +0,0 @@
-
-    /* To load this sql file and save these hashed passwords in the Users table:
-      mysql> use ttm-core;
-      Database changed
-      mysql> source 'user_load.sql';
-*/
-UPDATE ttm-core.Users SET User_Password='$2a$08$U0BvMOrT1zP31SUvM1Q6SedMqNtUpdVo49DjuJjG3Ff1WzCsALQma' WHERE User_ID='user';
-UPDATE ttm-core.Users SET User_Password='$2a$08$OwnnUpCXwdzFJQx0QwT/T.xWZdrWUzsBbZrNnfGh3ioOxdZrApzU.' WHERE User_ID='cecilia';
