ac0e5ee0496292ba5977a7dc096a2200453afa98
Karl Fogel <kfogel@red-bean.com>
2014-06-27 12:05:02 -0500

    Minor quoting and commenting fixes.
    * ajax/login_submit.php: Put username in single quotes when calling
      the SQL routine User__Find_Privileges().  Add a comment where some
      cookies are set in the admin case.
    
    These changes were ported manually from the old 'phpass' branch, so
    that the work now under way on the new 'allow-hashed-pw' branch can be
    soley about allowing hashed passwords and involve no other changes.

--- ajax/login_submit.php
+++ ajax/login_submit.php
@@ -24,7 +24,7 @@ if ($is_user>0){
        setcookie("user", $username, time() + 10800, '/');
       
        //now find which, if any, privileges they have and set an appropriate cookie
-       $privileges_query = "CALL User__Find_Privileges($username)";
+       $privileges_query = "CALL User__Find_Privileges('$username')";
        include ($_SERVER['DOCUMENT_ROOT'] . "/include/dbconnopen.php");
        $privileges = mysqli_query($cnnLISC, $privileges_query);
        
@@ -58,6 +58,8 @@ if ($is_user>0){
                    }
                }
                else{
+                   /* If user is an admin, unset the view_only and
+                      view_restricted cookies. */
                    setcookie('view_only', $level[0], time()-10800, '/');
                    setcookie('view_restricted', $level[0], time()-10800, '/');
                }
