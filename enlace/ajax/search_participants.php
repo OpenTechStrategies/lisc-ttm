2a8a37b0459f3c6c59f13da3f6c2dc008f21cf10
Cecilia Donnelly <donnelly.cecilia1@gmail.com>
2014-07-09 14:02:47 -0500

    Revert "Fix access query in several other files."
    This reverts commit 44120594f76c332da24e7d103aa47dea7e1a459b.

--- enlace/ajax/search_participants.php
+++ enlace/ajax/search_participants.php
@@ -7,7 +7,7 @@ include ($_SERVER['DOCUMENT_ROOT'] . "/include/dbconnopen.php");
 $user_sqlsafe=mysqli_real_escape_string($cnnLISC, $_COOKIE['user']);
 $get_program_access = "SELECT Program_Access FROM Users_Privileges INNER JOIN Users ON Users.User_Id = Users_Privileges.User_ID
     WHERE User_Email = '" . $user_sqlsafe . "'";
-//echo $get_program_access;
+echo $get_program_access;
 $program_access = mysqli_query($cnnLISC, $get_program_access);
 $prog_access = mysqli_fetch_row($program_access);
 $access = $prog_access[0];
