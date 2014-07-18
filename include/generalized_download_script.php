59f0624b6b798dc87714d2ac2ba663d966bbd689
Cecilia Donnelly <donnelly.cecilia1@gmail.com>
2014-07-18 11:54:24 -0500

    Add testing output to see why mysql table contents did not appear.
    When navigated to directly, the script worked correctly for a non-logged-in user
    as of last commit (i.e. returned the error page and didn't download anything).
    It worked fairly well for a logged-in user, except that it didn't retrieve the
    information correctly.  It did, however, download a csv file with the correct
    column headers!
    As of this commit, I am adding some testing output.  I suspect that either the
    dbconnopen file is not being included correctly or the query is not being
    read correctly.
    Issue #1.

--- include/generalized_download_script.php
+++ include/generalized_download_script.php
@@ -21,12 +21,16 @@ function generalized_download($dbconn_file, $database_conn, $query, $filename,
 
         // fetch the data
         include $dbconn_file;
+        echo "included file: " . $dbconn_file , "<br>"; //testing output
         $query_sqlsafe=mysqli_real_escape_string($database_conn, 
                 $query);
+        echo $query_sqlsafe; //testing output
+        echo "<br>" . "Connection variable: " . $database_conn;  //testing output
         $rows = mysql_query($database_conn, $query_sqlsafe);
 
         // loop over the rows, outputting them
-        while ($row = mysql_fetch_assoc($rows)) {
+        while ($row = mysql_fetch_row($rows)) {
+            print_r($row); //testing output
             fputcsv($output, $row);}
 
 
