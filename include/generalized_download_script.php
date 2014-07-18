b66bcbce497939fec9d5300ba02b5f1c86d2f2fe
Cecilia Donnelly <donnelly.cecilia1@gmail.com>
2014-07-18 16:16:53 -0500

    Use defined array to create exports.
    This commit creates the first element in a planned array of all exports across
    groups and includes a test case for downloading a file using the key to that
    array.
    Issue #1.

--- include/generalized_download_script.php
+++ include/generalized_download_script.php
@@ -6,31 +6,26 @@
 //code drawn from tutorial at:
 // http://code.stephenmorley.org/php/creating-downloadable-csv-files/
 
-function generalized_download($dbconn_file, $database_conn, $query, $filename,
-        $column_headers){
+function generalized_download($download_name){
+    $download_list_array=array('aldermans_records'=>array('db'=>'Bickerdike', 'query'=> 'SELECT * FROM Aldermanic_Records'));
+    print_r($download_list_array);//testing output
+    if (array_key_exists($download_name, $download_list_array)){
     if (isset($_COOKIE['user'])){
         // output headers so that the file is downloaded rather than displayed
         header('Content-Type: text/csv; charset=utf-8');
-        header('Content-Disposition: attachment; filename='.$filename);
+        header('Content-Disposition: attachment; filename='.$download_name);
 
         // create a file pointer connected to the output stream
         $output = fopen('php://output', 'w');
 
         // output the column headings
-        fputcsv($output, $column_headers);
+        //fputcsv($output, $column_headers);
 
         // fetch the data
-        include $dbconn_file;
-        //get correct connection variable:
-        if ($database_conn='cnnLISC'){$database_conn=$cnnLISC;}
-        elseif($database_conn='cnnBickerdike'){$database_conn=$cnnBickerdike;}
-        elseif($database_conn='cnnEnlace'){$database_conn=$cnnEnlace;}
-        elseif($database_conn='cnnLSNA'){$database_conn=$cnnLSNA;}
-        elseif($database_conn='cnnSWOP'){$database_conn=$cnnSWOP;}
-        elseif($database_conn='cnnTRP'){$database_conn=$cnnTRP;}
-        
-        $query_sqlsafe=mysqli_real_escape_string($database_conn, 
-                $query);
+        include '../'.$download_list_array[$download_name]['db'].'/include/dbconnopen.php';
+        $db_name= 'cnn' .$download_list_array[$download_name]['db'];
+        $database_conn=$$db_name;
+        $query_sqlsafe=$download_list_array[$download_name]['query'];
         $rows = mysqli_query($database_conn, $query_sqlsafe);
         
         // loop over the rows, outputting them
@@ -44,14 +39,15 @@ function generalized_download($dbconn_file, $database_conn, $query, $filename,
         include "error.html";
         exit;
     }
+    }
+    {
+        echo "This key does not exist.";
+        //create a new error page?  This should only happen when someone has
+        //done something nefarious.
+    }
 }
 include "dbconnopen.php";
 print_r($_POST);
-$conn_file=mysqli_real_escape_string($_POST['conn_file']);
-$conn_var=mysqli_real_escape_string($_POST['conn_var']);
-$query=mysqli_real_escape_string($_POST['query']);
-$filename=mysqli_real_escape_string($_POST['filename']);
-$title_array=unserialize($_POST['title_array']);
-generalized_download($conn_file, $conn_var, $query, 
-       $filename, $title_array);
+//generalized_download($_POST['download_name']);
+generalized_download('aldermans_records');
 ?>
