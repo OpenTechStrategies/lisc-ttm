bedd8df38f65fd411b75501aa8f7f4ce0379bd30
Cecilia Donnelly <donnelly.cecilia1@gmail.com>
2014-07-18 11:44:40 -0500

    Add download script file for use in all exports.
    I created a download script as a function in
    generalized_download_script.php.  I am not sure whether
    the headers will work as expected if used inside a function, since
    headers require nothing to have been printed before they are called.
    This file also includes a test call to the function, so we can see
    whether it works before adding the complications of calling it from
    another location.  This is the first step to finishing Issue #1.

---
+++ include/generalized_download_script.php
@@ -0,0 +1,43 @@
+<?php
+
+//takes as arguments the query, database connection and database connection
+//file, column headers, and file name.
+
+//code drawn from tutorial at:
+// http://code.stephenmorley.org/php/creating-downloadable-csv-files/
+
+function generalized_download($dbconn_file, $database_conn, $query, $filename,
+        $column_headers){
+    if (isset($_COOKIE['user'])){
+        // output headers so that the file is downloaded rather than displayed
+        header('Content-Type: text/csv; charset=utf-8');
+        header('Content-Disposition: attachment; filename='.$filename);
+
+        // create a file pointer connected to the output stream
+        $output = fopen('php://output', 'w');
+
+        // output the column headings
+        fputcsv($output, $column_headers);
+
+        // fetch the data
+        include $dbconn_file;
+        $query_sqlsafe=mysqli_real_escape_string($database_conn, 
+                $query);
+        $rows = mysql_query($database_conn, $query_sqlsafe);
+
+        // loop over the rows, outputting them
+        while ($row = mysql_fetch_assoc($rows)) {
+            fputcsv($output, $row);}
+
+
+        exit;
+    }
+    else{
+        include "error.html";
+        exit;
+    }
+}
+
+generalized_download('dbconnopen.php', $cnnLISC, "SELECT * FROM Privileges", 
+        "privilege_list_7-18-14.csv", array("Privilege_ID", "Privilege_Name"));
+?>
