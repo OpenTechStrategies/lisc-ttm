134cd4d3ef59e7b9b46b2e92875d88f3c8a5650e
Karl Fogel <kfogel@red-bean.com>
2014-07-18 15:43:47 -0700

    Fix an apparent copy-n-paste error.
    (Found during issue #19 work, but unrelated to that issue.)
    
    * bickerdike/ajax/add_sales_info.php: Fix variable name, by changing
      $add_bike_miles to $add_sales_data, and at the top of the file,
      remove the odd apology for the misleading variable names.
      (Presumably this code was copied from add_bike_info.php.)

--- bickerdike/ajax/add_sales_info.php
+++ bickerdike/ajax/add_sales_info.php
@@ -1,16 +1,16 @@
 <?php
 /*
- * Add new store information (sorry about the misleading variable names).
+ * Add new store information.
  * Add sales data and a date for a certain corner store (uses corner store ID, which is
  * dependent on another table).
  */
 
-$add_bike_miles = "INSERT INTO Funded_Organization_Records_Stores (Store_ID, Date, Sales_Data) VALUES
+$add_sales_data = "INSERT INTO Funded_Organization_Records_Stores (Store_ID, Date, Sales_Data) VALUES
                     ('" . $_POST['store'] ."',
                      '" . $_POST['date'] . "',
                      '" . $_POST['money'] . "')";
-echo $add_bike_miles;
+echo $add_sales_data;
 include "../include/dbconnopen.php";
-mysqli_query($cnnBickerdike, $add_bike_miles);
+mysqli_query($cnnBickerdike, $add_sales_data);
 include "../include/dbconnclose.php";
 ?>
