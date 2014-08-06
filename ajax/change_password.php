b91d21e62d299b55d7efdf2d560e8c40fdea9036
Karl Fogel <kfogel@red-bean.com>
2014-08-06 22:55:19 +0000

    For issue #2, store new passwords in hashed form.
    * ajax/change_password.php: When checking against the old password,
      accept it in either hashed or non-hashed form in the database, but
      store the new password hashed.

--- ajax/change_password.php
+++ ajax/change_password.php
@@ -1,36 +1,61 @@
 <?php
+require("../include/phpass-0.3/PasswordHash.php");
+$hasher=new PasswordHash(8, false);
+
 include ($_SERVER['DOCUMENT_ROOT'] . "/include/dbconnopen.php");
 if (isset($_POST['current_pw'])){
     $current_pw = $_POST['current_pw'];
 }
 $username = $_POST['username']; 
-$user_query = "SELECT * FROM  Users WHERE User_Email = '$username' AND User_Password = '$current_pw'";
-//echo $user_query;
+$user_query = "SELECT User_ID, User_Password FROM  Users WHERE User_Email = '$username'";
+$stored_hash="*";
 $user = mysqli_query($cnnLISC, $user_query);
-//print_r($user);
+$user_row = mysqli_fetch_row($user);
 $is_user = mysqli_num_rows($user);
+$user_id = $user_row[0];
+$found_pw = $user_row[1];
+
+/* For now, as per issue #2, we tolerate legacy passwords stored in
+ * non-hashed form in the database (although of course the new
+ * password we store will be in hashed form).  This shim could go
+ * away once all the passwords are hashed, but that's a manual step
+ * that we will do later.
+ */
+$cur_pw_non_hashed_match = ($current_pw == $found_pw);
+$cur_pw_hashed_match=$hasher->CheckPassword($current_pw, $found_pw);
+
 /*
  * Test to make sure that the username and password match:
  */
-if ($is_user>0){
+if ($is_user > 0 && ($cur_pw_non_hashed_match || $cur_pw_hashed_match))
+{
        /*if they match, then reset the password to the new password:
         */
         $new_pw = $_POST['new_pw'];
         $confirm_pw = $_POST['new_pw_2'];
         /*make sure the new and confirmed passwords match:*/
-        if ($new_pw == $confirm_pw){
-            $change_pw_query = "UPDATE Users SET User_Password ='". $_POST['new_pw'] . "' WHERE User_Email='$username'";
-            //echo $change_pw_query;
-            mysqli_query($cnnLISC, $change_pw_query);
-            echo "Password updated! <a href='/index.php'>Back to main site</a>";
+        if ($new_pw == $confirm_pw)
+        {
+            $new_hash = $hasher->HashPassword($new_pw);
+            if (strlen($new_hash) >= 20) 
+            {
+               $change_pw_query = "UPDATE Users SET User_Password ='". $new_hash . "' WHERE User_ID = $user_id";
+               mysqli_query($cnnLISC, $change_pw_query);
+               echo "Password updated! <a href='/index.php'>Back to main site</a>";
+            }
+            else
+            {
+               echo 'Unable to store new password&nbsp;&mdash;&nbsp;please <a href="/include/contact.php" >report this bug</a>.';
+            }
         }
-        else{
-            echo "The new passwords you entered do not match.  Please try again.";
+        else
+        {
+           echo "The new passwords you entered do not match.  Please try again.";
         }
 }
-else{
-    echo "The current password you entered doesn't seem to be correct.  Please try again.";
+else
+{
+        echo "The current password you entered doesn't seem to be correct.  Please try again.";
 }
-
 include ($_SERVER['DOCUMENT_ROOT'] . "/include/dbconnclose.php");
 ?>
