OpenTechStrategies/lisc-ttm

commit 218c14fbb44dce18aab72f55761793f34c88b0ed

Escape mysql inputs for core pages.
Added mysqli_real_escape_string to all mysql user inputs for pages that are
not group-specific (in top-level ajax, include, and index files).

ajax/edit_privileges.php
@@ -6,10 +6,16 @@
  * 
  * Privilege_ID refers to the site that is editing the user's information.
  */
-$edit_privilege_level = "UPDATE Users_Privileges SET Site_Privilege_ID='" . $_POST['privilege'] . "', Program_Access='".$_POST['program']."' WHERE
-    User_ID='" . $_POST['user'] . "' AND Privilege_Id='" . $_POST['site'] . "'";
-echo $edit_privilege_level;
 include "../include/dbconnopen.php";
+
+$user_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['user']);
+$site_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['site']);
+$privilege_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['privilege']);
+$program_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_POST['program']);
+
+$edit_privilege_level = "UPDATE Users_Privileges SET Site_Privilege_ID='" . $privilege_sqlsafe . "', Program_Access='".$program_sqlsafe."' WHERE
+    User_ID='" . $user_sqlsafe . "' AND Privilege_Id='" . $site_sqlsafe . "'";
+echo $edit_privilege_level;
 mysqli_query($cnnLISC, $edit_privilege_level);
 include "../include/dbconnclose.php";
 ?>

ajax/extend_staff_privilege.php
@@ -1,12 +1,15 @@
 <?php
+include "../include/dbconnopen.php";
+
+$username_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['username']);
+$password_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['password']);
 
 //add staff to login in lisc database as a whole
 $lisc_query = "INSERT INTO Users (User_Email, User_Password)
-    VALUES ('" . $_POST['username'] . "', '" . $_POST['password'] . "')";
+    VALUES ('" . $username_sqlsafe . "', '" . $password_sqlsafe . "')";
 
 
 //echo $lisc_query;
-include "../include/dbconnopen.php";
 mysqli_query($cnnLISC, $lisc_query);
 $user_id = mysqli_insert_id($cnnLISC);
 include "../include/dbconnopen.php";
@@ -14,6 +17,11 @@ include "../include/dbconnopen.php";
 /*
  * Give new user privileges by site.
  */
+
+$site_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['site']);
+$level_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['level']);
+$program_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_POST['program']);
+
 $privileges_query = "INSERT INTO Users_Privileges (User_ID, Privilege_ID, Site_Privilege_ID, Program_Access)
     VALUES ('" . $user_id . "', '" . $_POST['site'] . "', '" . $_POST['level'] . "', '" . $_POST['program'] . "')";
 //echo $privileges_query;

ajax/reset_password.php
@@ -2,9 +2,12 @@
 /*Resetting the password from the Alter Privileges page (e.g. for a user who has forgotten 
  * his/her password)
  */
-$reset_pw_query = "UPDATE Users SET User_Password='" . $_POST['pw'] . "' WHERE User_Id='" . $_POST['user'] . "'";
-echo $reset_pw_query;
+
 include "../include/dbconnopen.php";
+$pw_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['pw']);
+$user_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_POST['user']);
+$reset_pw_query = "UPDATE Users SET User_Password='" . $pw_sqlsafe . "' WHERE User_Id='" . $pw_user . "'";
+echo $reset_pw_query;
 mysqli_query($cnnLISC, $reset_pw_query);
 include "../include/dbconnclose.php";
 

include/add_staff.php
@@ -253,11 +253,11 @@ Add a new user to the system.
 			<option>-----</option>
   		  <?
                   /*List of users at this site.*/
-                  
- 		   $staff_list_query = "SELECT * FROM Users LEFT JOIN Users_Privileges ON 
- 		       (Users_Privileges.User_ID=Users.User_ID) WHERE Users_Privileges.Privilege_ID='" . $_COOKIE['sites'][0] . "'";
-  		  //echo $staff_list_query;
  		   include "../include/dbconnopen.php";
+                   $site_cookie_sqlsafe=mysqli_real_escape_string($cnnLISC, $_COOKIE['sites'][0]);
+ 		   $staff_list_query = "SELECT * FROM Users LEFT JOIN Users_Privileges ON 
+ 		       (Users_Privileges.User_ID=Users.User_ID) WHERE Users_Privileges.Privilege_ID='" . $site_cookie_sqlsafe . "'";
+  		  //echo $staff_list_query;
  		   $staff_list = mysqli_query($cnnLISC, $staff_list_query);
  		   print_r($staff_list);
  		   while ($staff=mysqli_fetch_array($staff_list)){

index.php
@@ -65,6 +65,7 @@ if ($_GET['action'] == 'logout') {
             $n = 0;
             while ($n < count($_COOKIE['sites'])) {
                 include "include/dbconnopen.php";
+                $privilege_id_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_COOKIE['sites'][$n]);
                 $get_privilege_name = "SELECT * FROM Privileges WHERE Privilege_Id='" . $_COOKIE['sites'][$n] . "'";
                 //echo $get_privilege_name;
                 $privilege_name = mysqli_query($cnnLISC, $get_privilege_name);

ajax/change_password.php
@@ -4,7 +4,10 @@ if (isset($_POST['current_pw'])){
     $current_pw = $_POST['current_pw'];
 }
 $username = $_POST['username']; 
-$user_query = "SELECT * FROM  Users WHERE User_Email = '$username' AND User_Password = '$current_pw'";
+$username_sqlsafe=mysqli_real_escape_string($cnnLISC, $username);
+$current_pw_sqlsafe=mysqli_real_escape_string($cnnLISC, $current_pw);
+$new_pw_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['new_pw']);
+$user_query = "SELECT * FROM  Users WHERE User_Email = '$username_sqlsafe' AND User_Password = '$current_pw_sqlsafe'";
 //echo $user_query;
 $user = mysqli_query($cnnLISC, $user_query);
 //print_r($user);
@@ -19,7 +22,7 @@ if ($is_user>0){
         $confirm_pw = $_POST['new_pw_2'];
         /*make sure the new and confirmed passwords match:*/
         if ($new_pw == $confirm_pw){
-            $change_pw_query = "UPDATE Users SET User_Password ='". $_POST['new_pw'] . "' WHERE User_Email='$username'";
+            $change_pw_query = "UPDATE Users SET User_Password ='". $new_pw_sqlsafe . "' WHERE User_Email='$username_sqlsafe'";
             //echo $change_pw_query;
             mysqli_query($cnnLISC, $change_pw_query);
             echo "Password updated! <a href='/index.php'>Back to main site</a>";

ajax/login_submit.php
@@ -6,7 +6,10 @@ if (isset($_POST['username'])){
     $password = $_POST["password"];
 }
 
-$user_query = "SELECT * FROM  Users WHERE User_Email = '$username' AND User_Password = '$password'";
+$username_sqlsafe=mysqli_real_escape_string($cnnLISC, $username);
+$password_sqlsafe=mysqli_real_escape_string($cnnLISC, $password);
+
+$user_query = "SELECT * FROM  Users WHERE User_Email = '$username_sqlsafe' AND User_Password = '$password_sqlsafe'";
 
 $user = mysqli_query($cnnLISC, $user_query);
 
@@ -16,7 +19,7 @@ $user_id = mysqli_fetch_array($user);
 //if this user exists in the database
 if ($is_user>0){
     //record this login in the Log
-    $log_call = "INSERT INTO Log (Log_Event) VALUES (CONCAT('" . $username . "', ' - Logged In'))";
+    $log_call = "INSERT INTO Log (Log_Event) VALUES (CONCAT('" . $username_sqlsafe . "', ' - Logged In'))";
     
     mysqli_query($cnnLISC, $log_call);
     
@@ -24,7 +27,7 @@ if ($is_user>0){
        setcookie("user", $username, time() + 10800, '/');
       
        //now find which, if any, privileges they have and set an appropriate cookie
-       $privileges_query = "CALL User__Find_Privileges('$username')";
+       $privileges_query = "CALL User__Find_Privileges('$username_sqlsafe')";
        $privileges = mysqli_query($cnnLISC, $privileges_query);
        
        $i=0;
@@ -39,7 +42,7 @@ if ($is_user>0){
                /*set a site cookie for each of the sites this person has access to.*/
                setcookie('sites['.$i.']', $privilege['Privilege_Id'], time() + 10800, '/');
                $get_level_of_access = "SELECT Site_Privilege_ID FROM Users_Privileges INNER JOIN Users
-                    ON Users_Privileges.User_ID=Users.User_ID WHERE User_Email = '$username' AND User_Password = '$password'";
+                    ON Users_Privileges.User_ID=Users.User_ID WHERE User_Email = '$username_sqlsafe' AND User_Password = '$password_sqlsafe'";
               
                $access_level = mysqli_query($cnnLISC, $get_level_of_access);
                $level = mysqli_fetch_row($access_level);
@@ -68,7 +71,7 @@ if ($is_user>0){
        }
 }
 else{
-    $log_call = "INSERT INTO Log (Log_Event) VALUES (CONCAT('" . $username . "', ' - Invalid Login'))";
+    $log_call = "INSERT INTO Log (Log_Event) VALUES (CONCAT('" . $username_sqlsafe . "', ' - Invalid Login'))";
     
     
     mysqli_query($cnnLISC, $log_call);

commit 3e59ef63294523e831b7566e158663ca1a03d4f4

Finish issue #19 core code work on Issue-19-sqlsafe branch.
* ajax/reset_password.php: Finish adding the _sqlsafe suffix.
  Along the way, fix a typo introduced in commit 218c14fbb,
  and remove a spurious echo that predates that commit.

* ajax/edit_privileges.php: Finish _sqlsafe suffix work, and
  likewise remove a spurious echo statement.

* ajax/extend_staff_privilege.php: Finish _sqlsafe suffix work.
  Also, actually use the SQL-sanitized variables that had been
  defined in commit 218c14fbb but not referred to.  Remove extra
  dbconnopen calls, and change final call to dbconnclose.

* include/add_staff.php: Finish _sqlsafe suffix work.  Also
  SQL-sanitize a site cookie in one place where that had been
  overlooked in commit 218c14fbb.  Remove dbconnopen calls
  that were commented out or were unnecessary.

* index.php: Finish _sqlsafe suffix work.  Also, use the
  $privilege_id_sqlsafe variable that had been defined in
  commit 218c14fbb but not used.

ajax/edit_privileges.php
@@ -13,9 +13,8 @@ $site_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['site']);
 $privilege_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['privilege']);
 $program_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_POST['program']);
 
-$edit_privilege_level = "UPDATE Users_Privileges SET Site_Privilege_ID='" . $privilege_sqlsafe . "', Program_Access='".$program_sqlsafe."' WHERE
+$edit_privilege_level_sqlsafe = "UPDATE Users_Privileges SET Site_Privilege_ID='" . $privilege_sqlsafe . "', Program_Access='" . $program_sqlsafe . "' WHERE
     User_ID='" . $user_sqlsafe . "' AND Privilege_Id='" . $site_sqlsafe . "'";
-echo $edit_privilege_level;
-mysqli_query($cnnLISC, $edit_privilege_level);
+mysqli_query($cnnLISC, $edit_privilege_level_sqlsafe);
 include "../include/dbconnclose.php";
 ?>

ajax/extend_staff_privilege.php
@@ -5,14 +5,11 @@ $username_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['username']);
 $password_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['password']);
 
 //add staff to login in lisc database as a whole
-$lisc_query = "INSERT INTO Users (User_Email, User_Password)
+$lisc_query_sqlsafe = "INSERT INTO Users (User_Email, User_Password)
     VALUES ('" . $username_sqlsafe . "', '" . $password_sqlsafe . "')";
 
-
-//echo $lisc_query;
-mysqli_query($cnnLISC, $lisc_query);
-$user_id = mysqli_insert_id($cnnLISC);
-include "../include/dbconnopen.php";
+mysqli_query($cnnLISC, $lisc_query_sqlsafe);
+$user_id_sqlsafe = mysqli_insert_id($cnnLISC);
 
 /*
  * Give new user privileges by site.
@@ -22,14 +19,13 @@ $site_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['site']);
 $level_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['level']);
 $program_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_POST['program']);
 
-$privileges_query = "INSERT INTO Users_Privileges (User_ID, Privilege_ID, Site_Privilege_ID, Program_Access)
-    VALUES ('" . $user_id . "', '" . $_POST['site'] . "', '" . $_POST['level'] . "', '" . $_POST['program'] . "')";
-//echo $privileges_query;
-include "../include/dbconnopen.php";
-mysqli_query($cnnLISC, $privileges_query);
-include "../include/dbconnopen.php";
+$privileges_query_sqlsafe = "INSERT INTO Users_Privileges (User_ID, Privilege_ID, Site_Privilege_ID, Program_Access)
+    VALUES ('" . $user_id_sqlsafe . "', '" . $site_sqlsafe . "', '" . $level_sqlsafe . "', '" . $program_sqlsafe . "')";
+
+mysqli_query($cnnLISC, $privileges_query_sqlsafe);
+include "../include/dbconnclose.php";
 
 
 
 ?>
-<span style="color:#990000; font-weight:bold;">Thank you for adding  <?echo $_POST['username'];?> to the database.</span>  
+<span style="color:#990000; font-weight:bold;">Thank you for adding  <?echo $username_sqlsafe;?> to the database.</span>  

ajax/reset_password.php
@@ -6,9 +6,8 @@
 include "../include/dbconnopen.php";
 $pw_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['pw']);
 $user_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_POST['user']);
-$reset_pw_query = "UPDATE Users SET User_Password='" . $pw_sqlsafe . "' WHERE User_Id='" . $pw_user . "'";
-echo $reset_pw_query;
-mysqli_query($cnnLISC, $reset_pw_query);
+$reset_pw_query_sqlsafe = "UPDATE Users SET User_Password='" . $pw_sqlsafe . "' WHERE User_Id='" . $user_sqlsafe . "'";
+mysqli_query($cnnLISC, $reset_pw_query_sqlsafe);
 include "../include/dbconnclose.php";
 
 ?>

include/add_staff.php
@@ -48,9 +48,8 @@ Add a new user to the system.
                     <option value="a">Access to all program information</option>
                     <?//get enlace programs
                     include "../enlace/include/dbconnopen.php";
-                     $get_all_programs="SELECT Programs.Program_ID, Name FROM Programs ORDER BY Name";
-                   // include "../include/dbconnopen.php";
-                    $all_programs=mysqli_query($cnnEnlace, $get_all_programs);
+                    $get_all_programs_sqlsafe="SELECT Programs.Program_ID, Name FROM Programs ORDER BY Name";
+                    $all_programs=mysqli_query($cnnEnlace, $get_all_programs_sqlsafe);
                     while ($program=mysqli_fetch_row($all_programs)){
                        ?><option value="<?echo $program[0];?>">
                 <?echo $program[1];?></option><?
@@ -74,9 +73,8 @@ Add a new user to the system.
                     <option value="a">Access to all program information</option>
                     <?//get enlace programs
                     include "../trp/include/dbconnopen.php";
-                     $get_all_programs="SELECT Programs.Program_ID, Program_Name FROM Programs ORDER BY Program_Name";
-                    include "../include/dbconnopen.php";
-                    $all_programs=mysqli_query($cnnTRP, $get_all_programs);
+                    $get_all_programs_sqlsafe="SELECT Programs.Program_ID, Program_Name FROM Programs ORDER BY Program_Name";
+                    $all_programs=mysqli_query($cnnTRP, $get_all_programs_sqlsafe);
                     while ($program=mysqli_fetch_row($all_programs)){
                        ?><option value="<?echo $program[0];?>">
                 <?echo $program[1];?></option><?
@@ -134,11 +132,12 @@ Add a new user to the system.
      * Draws list of existing users that are linked to this site (again, for admin users, it will
      * be the first site that they are linked to)
      */
-    $staff_list_query = "SELECT * FROM Users LEFT JOIN Users_Privileges ON 
-        (Users_Privileges.User_ID=Users.User_ID) WHERE Users_Privileges.Privilege_ID='" . $_COOKIE['sites'][0] . "'";
-    //echo $staff_list_query;
     include "../include/dbconnopen.php";
-    $staff_list = mysqli_query($cnnLISC, $staff_list_query);
+    $site_cookie_sqlsafe = mysqli_real_escape_string($cnnLISC, $_COOKIE['sites'][0]);
+    $staff_list_query_sqlsafe = "SELECT * FROM Users LEFT JOIN Users_Privileges ON 
+        (Users_Privileges.User_ID=Users.User_ID) WHERE Users_Privileges.Privilege_ID='" . $site_cookie_sqlsafe . "'";
+    //echo $staff_list_query_sqlsafe;
+    $staff_list = mysqli_query($cnnLISC, $staff_list_query_sqlsafe);
     print_r($staff_list);
     while ($staff=mysqli_fetch_array($staff_list)){
         echo $staff['User_ID'];
@@ -172,11 +171,10 @@ Add a new user to the system.
                     <?//get enlace programs
                     include "../enlace/include/dbconnopen.php";
                     
-                        $get_all_programs="SELECT Programs.Name, Programs.Program_ID FROM Programs 
+                    $get_all_programs_sqlsafe="SELECT Programs.Name, Programs.Program_ID FROM Programs
                            ORDER BY Name";
-                       // echo $get_all_programs;
-                    include "../include/dbconnopen.php";
-                    $all_programs=mysqli_query($cnnEnlace, $get_all_programs);
+                    // echo $get_all_programs_sqlsafe;
+                    $all_programs=mysqli_query($cnnEnlace, $get_all_programs_sqlsafe);
                     while ($program=mysqli_fetch_row($all_programs)){
                        ?> <option value="<?echo $program[1];?>">
                 <?echo $program[0];?></option><?
@@ -199,9 +197,8 @@ Add a new user to the system.
                     <option value="a">Access to all program information</option>
                     <?//get enlace programs
                     include "../trp/include/dbconnopen.php";
-                     $get_all_programs="SELECT Programs.Program_ID, Program_Name FROM Programs ORDER BY Program_Name";
-                    include "../include/dbconnopen.php";
-                    $all_programs=mysqli_query($cnnTRP, $get_all_programs);
+                    $get_all_programs_sqlsafe="SELECT Programs.Program_ID, Program_Name FROM Programs ORDER BY Program_Name";
+                    $all_programs=mysqli_query($cnnTRP, $get_all_programs_sqlsafe);
                     while ($program=mysqli_fetch_row($all_programs)){
                        ?><option value="<?echo $program[0];?>">
                 <?echo $program[1];?></option><?
@@ -255,10 +252,10 @@ Add a new user to the system.
                   /*List of users at this site.*/
  		   include "../include/dbconnopen.php";
                    $site_cookie_sqlsafe=mysqli_real_escape_string($cnnLISC, $_COOKIE['sites'][0]);
- 		   $staff_list_query = "SELECT * FROM Users LEFT JOIN Users_Privileges ON 
+ 		   $staff_list_query_sqlsafe = "SELECT * FROM Users LEFT JOIN Users_Privileges ON 
  		       (Users_Privileges.User_ID=Users.User_ID) WHERE Users_Privileges.Privilege_ID='" . $site_cookie_sqlsafe . "'";
-  		  //echo $staff_list_query;
- 		   $staff_list = mysqli_query($cnnLISC, $staff_list_query);
+  		  //echo $staff_list_query_sqlsafe;
+ 		   $staff_list = mysqli_query($cnnLISC, $staff_list_query_sqlsafe);
  		   print_r($staff_list);
  		   while ($staff=mysqli_fetch_array($staff_list)){
    		     echo $staff['User_ID'];

index.php
@@ -66,9 +66,9 @@ if ($_GET['action'] == 'logout') {
             while ($n < count($_COOKIE['sites'])) {
                 include "include/dbconnopen.php";
                 $privilege_id_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_COOKIE['sites'][$n]);
-                $get_privilege_name = "SELECT * FROM Privileges WHERE Privilege_Id='" . $_COOKIE['sites'][$n] . "'";
-                //echo $get_privilege_name;
-                $privilege_name = mysqli_query($cnnLISC, $get_privilege_name);
+                $get_privilege_name_sqlsafe = "SELECT * FROM Privileges WHERE Privilege_Id='" . $privilege_id_sqlsafe . "'";
+                //echo $get_privilege_name_sqlsafe;
+                $privilege_name = mysqli_query($cnnLISC, $get_privilege_name_sqlsafe);
                 $name = mysqli_fetch_array($privilege_name);
                 if ($name['Privilege_Name'] == 'Bickerdike') {
                     ?>