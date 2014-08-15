1f5942470c4f3282d81414ddc590a235666e2608
Cecilia Donnelly <donnelly.cecilia1@gmail.com>
2014-08-15 09:59:18 -0500

    Add La Casa as a TRP program.
    This commit adds a SQL file to create La Casa as a program profile
    in the TRP Programs table.
    Issue #5.

---
+++ trp/sql/change_trp_programs.sql
@@ -0,0 +1,11 @@
+/*
+ * This file will add a "La Casa" program to TRP's program table in accordance 
+ * with Issue #5.  This file should be run before deleting any programs,
+ * such that the La Casa program will have program ID equal to 6.
+*/
+
+
+USE ttm-trp;
+
+INSERT INTO Programs (Program_Name) VALUES ('La Casa');
+
