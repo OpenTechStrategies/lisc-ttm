416de5848d97cbfd5433c556bddaef9a5a216634
Cecilia Donnelly <donnelly.cecilia1@gmail.com>
2014-08-29 13:08:33 -0500

    Fix Issue #61 - hash passwords for new users.
    Added hashing to new passwords in ajax/extend_staff_privilege.php and
    ajax/reset_password.php.  Passwords for new users should now be saved as
    hashes and will pass the login test.
    
    Issue #61.

--- ajax/extend_staff_privilege.php
+++ ajax/extend_staff_privilege.php
@@ -1,27 +1,39 @@
 <?php
+require("../include/phpass-0.3/PasswordHash.php");
+$hasher=new PasswordHash(8, false);
 
+$new_password=$_POST['password'];
+$hash=$hasher->HashPassword($new_password);
+
+include "../include/dbconnopen.php";
+$username_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['username']);
 //add staff to login in lisc database as a whole
-$lisc_query = "INSERT INTO Users (User_Email, User_Password)
-    VALUES ('" . $_POST['username'] . "', '" . $_POST['password'] . "')";
+if (strlen($hash)>=20){
+    $lisc_query = "INSERT INTO Users (User_Email, User_Password)
+        VALUES ('" . $username_sqlsafe . "', '" . $hash . "')";
+    //echo $lisc_query;
+    mysqli_query($cnnLISC, $lisc_query);
+    $user_id = mysqli_insert_id($cnnLISC);
+    include "../include/dbconnclose.php";
 
-
-//echo $lisc_query;
-include "../include/dbconnopen.php";
-mysqli_query($cnnLISC, $lisc_query);
-$user_id = mysqli_insert_id($cnnLISC);
-include "../include/dbconnopen.php";
-
-/*
- * Give new user privileges by site.
- */
-$privileges_query = "INSERT INTO Users_Privileges (User_ID, Privilege_ID, Site_Privilege_ID, Program_Access)
-    VALUES ('" . $user_id . "', '" . $_POST['site'] . "', '" . $_POST['level'] . "', '" . $_POST['program'] . "')";
-//echo $privileges_query;
-include "../include/dbconnopen.php";
-mysqli_query($cnnLISC, $privileges_query);
-include "../include/dbconnopen.php";
-
-
+    /*
+     * Give new user privileges by site.
+     */
+    $privileges_query = "INSERT INTO Users_Privileges (User_ID, Privilege_ID, 
+        Site_Privilege_ID, Program_Access)
+        VALUES ('" . $user_id . "', '" . $_POST['site'] . "', '" .
+        $_POST['level'] . "', '" . $_POST['program'] . "')";
+    //echo $privileges_query;
+    include "../include/dbconnopen.php";
+    mysqli_query($cnnLISC, $privileges_query);
+    include "../include/dbconnclose.php";
+}
+else
+{
+   echo 'Unable to store new password&nbsp;&mdash;&nbsp;please '
+    . '<a href="/include/contact.php" >report this bug</a>.';
+}
 
 ?>
-<span style="color:#990000; font-weight:bold;">Thank you for adding  <?echo $_POST['username'];?> to the database.</span>  
+<span style="color:#990000; font-weight:bold;">Thank you for adding 
+    <?echo $_POST['username'];?> to the database.</span>  
--- ajax/reset_password.php
+++ ajax/reset_password.php
@@ -1,11 +1,25 @@
 <?php
+require("../include/phpass-0.3/PasswordHash.php");
+$hasher=new PasswordHash(8, false);
 /*Resetting the password from the Alter Privileges page (e.g. for a user who has forgotten 
  * his/her password)
  */
-$reset_pw_query = "UPDATE Users SET User_Password='" . $_POST['pw'] . "' WHERE User_Id='" . $_POST['user'] . "'";
-echo $reset_pw_query;
+
 include "../include/dbconnopen.php";
-mysqli_query($cnnLISC, $reset_pw_query);
-include "../include/dbconnclose.php";
+$new_password=$_POST['pw'];
+$username_sqlsafe=mysqli_real_escape_string($cnnLISC, $_POST['user']);
+$hash=$hasher->HashPassword($new_password);
+            if (strlen($hash)>=20){
+                $change_pw_query = "UPDATE Users SET User_Password ='". $hash 
+                        . "' WHERE User_ID='" . $username_sqlsafe . "'";
+                mysqli_query($cnnLISC, $change_pw_query);
+                include "../include/dbconnclose.php";
+            }
+            else
+            {
+               echo 'Unable to store new password&nbsp;&mdash;&nbsp;please '
+                . '<a href="/include/contact.php" >report this bug</a>.';
+            }
+
 
 ?>
