e2e61e937fea5af9882ab86c06bd523905d0dc3a
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2014-09-25 12:09:33 -0500

    Test calculating dosage without pushing it to the array.
    Issue #1.  After the last commit (a047a90), the browser couldn't find
    the dosage exports.  This suggests that the if statement is now
    working correctly, but something within it is causing a problem in the
    creation of the file.  This commit comments out a few lines within the
    if statement and adds testing output.

--- include/generalized_download_script.php
+++ include/generalized_download_script.php
@@ -1932,7 +1932,7 @@ INNER JOIN Programs ON Programs.Program_Id=Session_Names.Program_ID',
         
         
         $rows = mysqli_query($database_conn, $query_sqlsafe);
-        include "../include/dosage_percentage.php"; //only included for Enlace dosage calculation
+        include "../enlace/include/dosage_percentage.php"; //only included for Enlace dosage calculation
         // loop over the rows, outputting them
         while ($row = mysqli_fetch_row($rows)) {
             // if this is the Enlace dosage export, we need to calculate dosage
