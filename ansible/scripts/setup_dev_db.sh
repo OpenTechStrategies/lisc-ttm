75e670ed7a334e4bbf58fd3f39cc8e061baa24bc
Christopher Allan Webber <cwebber@dustycloud.org>
2014-12-19 16:32:14 -0600

    Once I moved the ansible repo into this, this script location changed
    Repairing the script location for the new setup.

--- ansible/scripts/setup_dev_db.sh
+++ ansible/scripts/setup_dev_db.sh
@@ -7,7 +7,7 @@ for name in core bickerdike enlace lsna swop trp; do
     sudo mysql -u root < ${name}/sql/ttm-${name}_routines.sql;
 done
 
-sudo mysql -u root < /vagrant/scripts/test_permissions.sql
+sudo mysql -u root < /vagrant/ansible/scripts/test_permissions.sql
 
 sudo mysql -u root < data/lisc-ttm-sample-data.sql
 
