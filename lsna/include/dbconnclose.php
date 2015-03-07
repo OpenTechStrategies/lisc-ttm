9c384299d91a195633cb3f7e56d94dec698c826b
Christopher Allan Webber <cwebber@dustycloud.org>
2015-03-07 17:42:40 -0600

    Issue #78: Update old dbconnclose files to use new connection shutdown code

--- lsna/include/dbconnclose.php
+++ lsna/include/dbconnclose.php
@@ -1,4 +1,6 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] . "/core/tools/db.php";
+
 // Close db connection
-mysqli_close($cnnLSNA);
+maybe_close_dbconn("LSNA");
 ?>
--- trp/include/dbconnclose.php
+++ trp/include/dbconnclose.php
@@ -1,11 +1,6 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'] . "/include/dbconnopen.php");
-include_once($_SERVER['DOCUMENT_ROOT'] . "/core/include/setup_user.php");
+include_once $_SERVER['DOCUMENT_ROOT'] . "/core/tools/db.php";
 
-user_enforce_has_access($TRP_id);
-
-?>
-<?php
 // Close db connection
-mysqli_close($cnnTRP);
+maybe_close_dbconn("TRP");
 ?>
