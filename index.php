c0aa0195675559ba18e3817e6bad0ab601e682a7
Christopher Allan Webber <cwebber@dustycloud.org>
2015-03-08 21:42:12 -0500

    #78: Starting to convert the index page
    Includes removing an "if" section that seems to have been unused

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] . "/core/tools/auth.php";
+
 //if action is logout
 if ($_GET['action'] == 'logout') {
-    include_once $_SERVER['DOCUMENT_ROOT'] . "/core/tools/auth.php";
-    $loggingout = pleaseLogOut($_COOKIE['PHPSESSID']);
+    $loggingout = pleaseLogOut();
     if ($loggingout){
-        //kill cookie
-        setcookie('PHPSESSID', '', time() - 3600, '/');
-        setcookie('user', '', time() - 3600, '/');
-        setcookie('page', '', time() - 3600, '/');
-        setcookie('participant', '', time() - 3600, '/');
-        setcookie('sites[0]', '', time() - 3600, '/');
-        setcookie('sites[1]', '', time() - 3600, '/');
-        setcookie('sites[2]', '', time() - 3600, '/');
-        setcookie('sites[3]', '', time() - 3600, '/');
-        setcookie('sites[4]', '', time() - 3600, '/');
-        setcookie('sites[5]', '', time() - 3600, '/');
-        setcookie('sites[6]', '', time() - 3600, '/');
-        setcookie('sites[7]', '', time() - 3600, '/');
-        setcookie('view_restricted', '', time() - 3600, '/');
-        setcookie('view_only', '', time() - 3600, '/');
         //redirect
         header('Location: /index.php');
     }
@@ -32,7 +18,7 @@ if ($_GET['action'] == 'logout') {
     <?php
     include "header.php";
 
-    if (!isset($_COOKIE['user'])) {
+    if (!isLoggedIn()) {
         //if no one is logged in, then send the user to the login page.  This also happens 
         //when the user cookie expires.
 
@@ -62,49 +48,42 @@ if ($_GET['action'] == 'logout') {
 
         <?php
         echo "Thank you for logging in.<br>";
-        if (in_array('all_sites', $_COOKIE['sites'])) {
-            /*
-             * Didn't end up using this option.
-             */
-            echo "You have access to all sites.";
-        } else {
-            /*
-             * Count through the sites where this user has access.
-             */
-            $n = 0;
-            while ($n < count($_COOKIE['sites'])) {
-                include "include/dbconnopen.php";
-                $privilege_id_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_COOKIE['sites'][$n]);
-                $get_privilege_name_sqlsafe = "SELECT * FROM Privileges WHERE Privilege_Id='" . $privilege_id_sqlsafe . "'";
-                //echo $get_privilege_name_sqlsafe;
-                $privilege_name = mysqli_query($cnnLISC, $get_privilege_name_sqlsafe);
-                $name = mysqli_fetch_array($privilege_name);
-                if ($name['Privilege_Name'] == 'Bickerdike') {
-                    ?>
-                    You have access to the <a href="/bickerdike/">Bickerdike</a> site information. <br>
-                    <?php
-                } elseif ($name['Privilege_Name'] == 'Logan Square Neighborhood Association') {
-                    ?>
-                    You have access to the <a href="/lsna/">LSNA</a> site information. <br>
-                    <?php
-                } else if ($name['Privilege_Name'] == 'The Resurrection Project') {
-                    ?>
-                    You have access to <a href="/trp/">The Resurrection Project</a> site information. <br/>
-                    <?php
-                } else if ($name['Privilege_Name'] == 'Southwest Organizing Project') {
-                    ?>
-                    You have access to the <a href="/swop/">Southwest Organizing Project</a> site information. <br/>
-                    <?php
-                } else if ($name['Privilege_Name'] == 'Enlace') {
-                    ?>
-                    You have access to the <a href="/enlace/">Enlace</a> site information. <br/>
-                    <?php
-                }
+        /*
+         * Count through the sites where this user has access.
+         */
+        $n = 0;
+        while ($n < count($_COOKIE['sites'])) {
+            include "include/dbconnopen.php";
+            $privilege_id_sqlsafe=  mysqli_real_escape_string($cnnLISC, $_COOKIE['sites'][$n]);
+            $get_privilege_name_sqlsafe = "SELECT * FROM Privileges WHERE Privilege_Id='" . $privilege_id_sqlsafe . "'";
+            //echo $get_privilege_name_sqlsafe;
+            $privilege_name = mysqli_query($cnnLISC, $get_privilege_name_sqlsafe);
+            $name = mysqli_fetch_array($privilege_name);
+            if ($name['Privilege_Name'] == 'Bickerdike') {
                 ?>
+                You have access to the <a href="/bickerdike/">Bickerdike</a> site information. <br>
                 <?php
-                include "include/dbconnclose.php";
-                $n = $n + 1;
+            } elseif ($name['Privilege_Name'] == 'Logan Square Neighborhood Association') {
+                ?>
+                You have access to the <a href="/lsna/">LSNA</a> site information. <br>
+                <?php
+            } else if ($name['Privilege_Name'] == 'The Resurrection Project') {
+                ?>
+                You have access to <a href="/trp/">The Resurrection Project</a> site information. <br/>
+                <?php
+            } else if ($name['Privilege_Name'] == 'Southwest Organizing Project') {
+                ?>
+                You have access to the <a href="/swop/">Southwest Organizing Project</a> site information. <br/>
+                <?php
+            } else if ($name['Privilege_Name'] == 'Enlace') {
+                ?>
+                You have access to the <a href="/enlace/">Enlace</a> site information. <br/>
+                <?php
             }
+            ?>
+            <?php
+            include "include/dbconnclose.php";
+            $n = $n + 1;
         }
     }
     ?>
