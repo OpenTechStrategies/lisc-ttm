a01569965bf6d4aa2579f7f79ff5848eccab3282
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-03-31 10:38:31 -0500

    #5: Add new tables to sample data

--- data/lisc-ttm-sample-data.sql
+++ data/lisc-ttm-sample-data.sql
@@ -929,6 +929,8 @@ CREATE TABLE `Participants` (
   `Neighborhood` varchar(100) DEFAULT NULL,
   `Eval_ID` int(11) DEFAULT NULL,
   `CPS_ID` varchar(45) DEFAULT NULL,
+  `Email_2` varchar(60),      
+  `Mobile_Phone` varchar(45),
   PRIMARY KEY (`Participant_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -1280,6 +1282,101 @@ LOCK TABLES `Teachers` WRITE;
 /*!40000 ALTER TABLE `Teachers` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Teachers` ENABLE KEYS */;
 UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `Colleges`;
+
+CREATE TABLE `Colleges`
+(
+`College_ID` int(11) NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (`College_ID`),
+`College_Name` varchar(100),
+`College_Type` varchar(100)
+) ENGINE=InnoDB;
+
+
+DROP TABLE IF EXISTS `La_Casa_Basics`;
+
+CREATE TABLE `La_Casa_Basics`
+(
+`Student_ID` int(11) NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (`Student_ID`),
+`Participant_ID_Students` int(11),
+INDEX `par_ind_stu` (`Participant_ID_Students`),
+FOREIGN KEY (`Participant_ID_Students`) REFERENCES `Participants`
+    (`Participant_ID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+`Household_Size` int(3),
+`Parent1_AGI` int(11),
+`Parent2_AGI` int(11),
+`Student_AGI` int(11),
+`ACT_Score` int(5),
+`High_School_GPA` varchar(30),
+`Dependency_Status` varchar(10),
+`Father_Highest_Level_Education` int(11),
+`Mother_Highest_Level_Education` int(11),
+`Student_Aspiration` int(11),
+`First_Generation_College_Student` varchar(30),
+`Persistence_Graduation` varchar(30),
+`Student_Hometown` varchar(100),
+`Student_High_School` varchar(100),
+`Scholarship_Apps` int(11),
+`Scholarship_Num` int(11),
+`Scholarship_Volume` int(11),
+`Scholarships_Received` int(11),
+`Household_Income` int(11),
+`AMI` int(11),
+`Move_In_Date` date,
+`Move_Out_Date` date,
+`Mid_Twenties` int(11),
+`Masters_Degree` int(11),
+`Married` int(11),
+`Has_Children` int(11),
+`Homeless` int(11),
+`Self_Sustaining` int(11)
+
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `LC_Terms`;
+
+CREATE TABLE LC_Terms
+(
+`Term_ID` int(11) NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (`Term_ID`),
+`Participant_ID` int(11),
+INDEX `student_by_term` (`Participant_ID`),
+FOREIGN KEY (`Participant_ID`) REFERENCES `Participants`
+    (`Participant_ID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+`College_ID` int(11), 
+`Term_Type` varchar(45),
+`Term` varchar(45),
+`School_Year` int(4),
+`Credits` varchar(45),
+`Loan_Applications` int(3),
+`Loan_Volume` varchar(45),
+`Loans_Received` varchar(45),
+`Major` varchar(100),
+`College_Match` varchar(30),
+`College_GPA` varchar(30),
+`Internship_Status` int(11),
+`Intern_Hours` int(11)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `Educational_Levels`;
+
+CREATE TABLE `Educational_Levels`
+(
+`Education_ID` int(11) NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (`Education_ID`),
+`Education_Level_Name` varchar(45)
+) ENGINE = InnoDB;
+
+INSERT INTO `Educational_Levels` (Education_Level_Name) VALUES ('Elementary'), ('Middle School'), ('Some HS'), ('GED'), ('High School Diploma'), ('Some College'), ('Trade School'), ('Associates Degree'), ('Bachelors Degree'), ('Masters Degree'), ('MD'), ('PhD'), ('Unknown');
+
+
+
 
 --
 -- Current Database: `ttm-core`
