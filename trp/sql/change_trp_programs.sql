373532217eb95254f56697d0f328a9311ac5e70c
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-05-05 16:13:44 -0500

    Add license header to all TRP files

--- trp/sql/change_trp_programs.sql
+++ trp/sql/change_trp_programs.sql
@@ -1,4 +1,21 @@
 /*
+ *   TTM is a web application to manage data collected by community organizations.
+ *   Copyright (C) 2014, 2015  Local Initiatives Support Corporation (lisc.org)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
  * This file will add a "La Casa" program to TRP's program table in accordance 
  * with Issue #5.  This file should be run before deleting any programs,
  * such that the La Casa program will have program ID equal to 6.
