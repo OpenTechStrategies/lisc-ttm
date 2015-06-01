e59a75f968e23ea551bdabdea04913a5cdd6717a
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-06-01 17:12:05 -0500

    #117: Add a funders report
    Update the funders report so that it returns results with any, all, or none of
    the filters applied.  The report is not aesthetically pleasing at the moment,
    but functions as requested.

--- lsna/reports/funders.php
+++ lsna/reports/funders.php
@@ -14,7 +14,10 @@ Start date: <input type="text" class="hadDatepicker" name = "start_date"> <br>
 $institution_list = "SELECT Institution_ID, Institution_Name FROM Institutions LEFT JOIN Institution_Types on Institution_Type = Institution_Type_ID WHERE Institution_Type_Name = 'Funder'";
 include "../include/dbconnopen.php";
 $funders_result_sqlsafe = mysqli_query($cnnLSNA, $institution_list);
+$funder_array = array();
 while ($funder = mysqli_fetch_row($funders_result_sqlsafe)){
+    //also make array of funders for reference below
+    $funder_array[$funder[0]] = $funder[1];
     ?>
     <input type="checkbox" id="" name = "funder[]"value = <?php echo $funder[0];?>><label for=""><?php echo $funder[1];?></label><br>
 <?php
@@ -22,29 +25,56 @@ while ($funder = mysqli_fetch_row($funders_result_sqlsafe)){
 ?>
 <input type = "submit" value = "Filter" name = "funder_submit">
 </form>
-
-<table  class="program_involvement_table">
-<tr><th>Activity Name</th><th>Date</th><th>Program/Campaign</th><th>Funder</th></tr>
+<br />
+<br />
 <?php
 $start_date = '2015-01-01';
 $end_date = '2015-07-01';
-$funder_string = '';
+$date_string = "";
+$funder_string = "";
 if (isset($_POST['funder_submit'])){
+?>
+     <h5> Current search based on: </h5>
+<?php
     if ($_POST['start_date'] != ''){
         $reorder_start_date = explode('-', $_POST['start_date']);
         $start_date = $reorder_start_date[2] . '-' . $reorder_start_date[0] . '-' . $reorder_start_date[1];
+        $start_string = " AND Date >= '$start_date' ";
+        echo "<br /><strong>After: </strong>" . $_POST['start_date'];
     }
     if ($_POST['end_date'] != ''){
         $reorder_end_date = explode('-', $_POST['end_date']);
         $end_date = $reorder_end_date[2] . '-' . $reorder_end_date[0] . '-' . $reorder_end_date[1];
+        $end_string = " AND Date <= '$end_date' ";
+        echo "<br /><strong>Before: </strong> " . $_POST['end_date'];
     }
-    foreach ($_POST['funder'] as $funder){
-        $funder_string = " AND Funder_ID = $funder"; 
+    if ($_POST['funder'] != ''){
+        echo "<br /> <strong> Funded by: </strong>";
+        $funder_string = " AND (";
+        foreach ($_POST['funder'] as $funder){
+            if ($funder != $_POST['funder'][0]) {
+                echo " or ";
+                $funder_string .= " OR Funder_ID = $funder"; 
+            }
+            else {
+                $funder_string .= "  Funder_ID = $funder"; 
+            }
+            echo $funder_array[$funder];
+        }
+        $funder_string .=")";
     }
-
 }
 
-$funder_schedule_query = "SELECT Activity_Name, Date, Subcategory_Name, Institution_Name FROM Subcategory_Dates LEFT JOIN Subcategories ON Subcategory_Dates.Subcategory_ID = Subcategories.Subcategory_ID LEFT JOIN Institutions ON Funder_ID = Institution_ID WHERE Date >= '$start_date' AND Date <= '$end_date' $funder_string ORDER BY Subcategory_Dates.Subcategory_ID;";
+$funder_schedule_query = "SELECT Activity_Name, Date, Subcategory_Name, Institution_Name FROM Subcategory_Dates LEFT JOIN Subcategories ON Subcategory_Dates.Subcategory_ID = Subcategories.Subcategory_ID LEFT JOIN Institutions ON Funder_ID = Institution_ID WHERE Wright_College_Program_Date_ID IS NOT NULL $start_string $end_string $funder_string ORDER BY Subcategory_Dates.Subcategory_ID;";
+
+
+?>
+
+<br />
+<br />
+<table  class="program_involvement_table">
+<tr><th>Activity Name</th><th>Date</th><th>Program/Campaign</th><th>Funder</th></tr>
+<?php
 include "../include/dbconnopen.php";
 $funder_schedule = mysqli_query($cnnLSNA, $funder_schedule_query);
 while ($row = mysqli_fetch_row($funder_schedule)){
--- lsna/reports/reports.php
+++ lsna/reports/reports.php
@@ -96,7 +96,10 @@ if (isset($_POST['satisfaction_program'])) {
 <div class="content">
     <h3>Reports</h3><hr/><br/>
     <a href="query.php" class="report_tab">Participant Query Search</a>&nbsp;&nbsp; <a href="survey_query.php" class="report_tab">Survey Query Search</a>
-    <a href="issue_events_report.php" class="report_tab">Services Rendered Report</a><br>
+    <a href="issue_events_report.php" class="report_tab">Services Rendered Report</a>
+    <a class="report_tab" id="involvement_tab" href="funders.php">Funders</a>
+
+<br>
     <a class="report_tab" id="pm_survey_tab" href="javascript:;" onclick="
             $('#parent_mentor_survey').show();
             $('#parent_mentor_survey_pre_post').slideUp();
@@ -107,7 +110,6 @@ if (isset($_POST['satisfaction_program'])) {
             $('.report_tab').removeClass('selected');
             $(this).addClass('selected');
        ">Parent Mentor Surveys</a>
-
     <a class="report_tab" id="involvement_tab" href="program_involvement.php">Program and Campaign Involvement</a>
 
     <a class="report_tab" id="roles_tab" href="participant_roles.php">Participant Roles</a>
@@ -157,13 +159,13 @@ if (isset($_POST['satisfaction_program'])) {
     <br/>
 
 
-    <?php
-    include "pm_survey_results.php";
-    include "teacher_surveys.php";
-    include "teacher_surveys_pre_post.php";
-    include "satisfaction_surveys.php";
-    include "pm_attendance.php";
-    ?>
+<?php
+include "pm_survey_results.php";
+include "teacher_surveys.php";
+include "teacher_surveys_pre_post.php";
+include "satisfaction_surveys.php";
+include "pm_attendance.php";
+?>
 
 </div>
 <br/><br/>
